1d32408468fa08f085862c3e2e28f065c14dbecc
Ward Price <wardprice@gmail.com>
2020-06-28 19:34:14 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -3,7 +3,6 @@ def count_strings(array)
   array.count |element|
     element.class = String
   end
-end
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
