2cc20d5b796a3b7d1bd4e01167c67185421f1f69
Ward Price <wardprice@gmail.com>
2020-06-28 19:32:32 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,6 +1,8 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
-  
+  array.count do 
+    String
+  end
 end
 
 def count_empty_strings(array)
